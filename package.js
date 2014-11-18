03484f63a059c62039c04c816404475a026433af
Joshua Ohlman <codewriterohs@gmail.com>
2014-11-18 11:19:43 -0600

    Prep for meteor 1.0 version

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
   summary: "The mobile-friendly, responsive, and lightweight jQuery date & time input picker."
+  , name: 'cwohlman:pickadate'
+  , git: 'http://github.com/usefulio/meteor-pickadate.git'
+  , version: '0.0.0'
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom('METEOR@1.0');
   api.use('jquery', 'client');
   api.add_files([
     'js/picker.js',
@@ -13,4 +17,4 @@ Package.on_use(function (api, where) {
     'css/classic.date.css',
     'css/classic.time.css',
   ], 'client');
-})
+});
